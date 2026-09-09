b18909a1feae14d91b789907914d869f3c343963
Masionluxury-hub <don.storeinfo1@gmail.com>
2026-09-09 01:43:18 -0500

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -1,13 +1,99 @@
-// Catálogo publicado. Copia una ficha, cambia sus datos y sube su imagen a /catalogo.
+// CatÃ¡logo publicado. Las imÃ¡genes estÃ¡n en /catalogo.
 window.MAISON_CATALOG = [
-  /*
   {
-    id: "referencia-unica",
-    name: "Nombre de la fragancia",
-    reference: "ML-001",
-    family: "Oriental",
-    price: "COP $180.000",
-    photo: "catalogo/referencia-unica.webp"
+    "id": "5yi2f6vr",
+    "name": "THANK U NEXT ( ARIANA GRANDE",
+    "reference": "",
+    "family": "Floral",
+    "price": "100.000",
+    "photo": "catalogo/1000504316.webp"
+  },
+  {
+    "id": "raqwar2c",
+    "name": "HAWAS ELIXIR",
+    "reference": "",
+    "family": "Amaderado",
+    "price": "110.000",
+    "photo": "catalogo/1000504321.webp"
+  },
+  {
+    "id": "tsq2afk7",
+    "name": "ERBA PURA (XERJOFF)",
+    "reference": "",
+    "family": "Amaderado",
+    "price": "110.000",
+    "photo": "catalogo/1000504328.webp"
+  },
+  {
+    "id": "u0nl2sou",
+    "name": "MOSCHINO TOY 2 BUBBLE",
+    "reference": "",
+    "family": "Floral",
+    "price": "90.000",
+    "photo": "catalogo/1000504311.webp"
+  },
+  {
+    "id": "rr3anica",
+    "name": "OUD FOR GLORY (BLACK)",
+    "reference": "",
+    "family": "Amaderado",
+    "price": "110.000",
+    "photo": "catalogo/1000504309.webp"
+  },
+  {
+    "id": "dzm8v8w3",
+    "name": "NITRO RED",
+    "reference": "",
+    "family": "CÃ­trico",
+    "price": "120.000",
+    "photo": "catalogo/1000504324.webp"
+  },
+  {
+    "id": "l7qndxws",
+    "name": "CLUB DE NUIT",
+    "reference": "",
+    "family": "CÃ­trico",
+    "price": "110.000",
+    "photo": "catalogo/1000504323.webp"
+  },
+  {
+    "id": "pbfdrdj2",
+    "name": "LA BOMBA (CAROLINA HERRERA)",
+    "reference": "",
+    "family": "Floral",
+    "price": "100.000",
+    "photo": "catalogo/1000504310.webp"
+  },
+  {
+    "id": "kwc10309",
+    "name": "AMETHYST",
+    "reference": "",
+    "family": "Amaderado",
+    "price": "110.000",
+    "photo": "catalogo/1000504301.webp"
+  },
+  {
+    "id": "7wxicy6y",
+    "name": "KHAMRAH QAHWA",
+    "reference": "",
+    "family": "Oriental",
+    "price": "100.000",
+    "photo": "catalogo/1000504315.webp"
+  },
+  {
+    "id": "wf11tbvf",
+    "name": "BADE'E AL OUD",
+    "reference": "",
+    "family": "FougÃ¨re",
+    "price": "120.000",
+    "photo": "catalogo/1000504307.webp"
+  },
+  {
+    "id": "l4f2cu3l",
+    "name": "Amber Rouge",
+    "reference": "",
+    "family": "Oriental",
+    "price": "120.000",
+    "photo": "catalogo/1000504306.webp"
   }
-  */
 ];
