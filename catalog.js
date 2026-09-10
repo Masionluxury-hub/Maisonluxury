647f126f9c2b468a6b49c7dc7e25ead27cefd346
Masionluxury-hub <don.storeinfo1@gmail.com>
2026-09-09 22:30:25 -0500

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -1,6 +1,198 @@
 // CatÃ¡logo publicado. Las imÃ¡genes estÃ¡n en /catalogo.
 window.MAISON_CATALOG = [
   {
+    "id": "wu1ji218",
+    "name": "PISA",
+    "reference": "",
+    "family": "Chipre",
+    "price": "125.000",
+    "photo": "catalogo/1000506234.webp"
+  },
+  {
+    "id": "jcghgo9n",
+    "name": "ELIXIR 9 PM",
+    "reference": "",
+    "family": "Amaderado",
+    "price": "100.000",
+    "photo": "catalogo/1000506232.webp"
+  },
+  {
+    "id": "zu0kxb7j",
+    "name": "MUSAMAN",
+    "reference": "",
+    "family": "Oriental",
+    "price": "120.000",
+    "photo": "catalogo/1000506230.webp"
+  },
+  {
+    "id": "qlp7fvn2",
+    "name": "FAKAR BLACK",
+    "reference": "",
+    "family": "Oriental",
+    "price": "120.000",
+    "photo": "catalogo/1000506228.webp"
+  },
+  {
+    "id": "8cscd7s0",
+    "name": "REBEL 9 PM",
+    "reference": "",
+    "family": "Amaderado",
+    "price": "110.000",
+    "photo": "catalogo/1000506226.webp"
+  },
+  {
+    "id": "17h7gdgg",
+    "name": "KARPOS",
+    "reference": "",
+    "family": "Chipre",
+    "price": "125.000",
+    "photo": "catalogo/1000506225.webp"
+  },
+  {
+    "id": "c912dbyg",
+    "name": "OMBRE NOMADE",
+    "reference": "",
+    "family": "Amaderado",
+    "price": "105.000",
+    "photo": "catalogo/1000506223.webp"
+  },
+  {
+    "id": "akldq6yz",
+    "name": "ONE LUCKY",
+    "reference": "",
+    "family": "Chipre",
+    "price": "80.000",
+    "photo": "catalogo/1000506222.webp"
+  },
+  {
+    "id": "kvpx199q",
+    "name": "LEMALE ELIXIR",
+    "reference": "",
+    "family": "Amaderado",
+    "price": "115.000",
+    "photo": "catalogo/1000506167.webp"
+  },
+  {
+    "id": "lw2ifo40",
+    "name": "EROS FLAME",
+    "reference": "",
+    "family": "Chipre",
+    "price": "80.000",
+    "photo": "catalogo/1000506163.webp"
+  },
+  {
+    "id": "z02a2q3k",
+    "name": "BLACK XS",
+    "reference": "",
+    "family": "Amaderado",
+    "price": "80.000",
+    "photo": "catalogo/1000506161.webp"
+  },
+  {
+    "id": "a85c5izx",
+    "name": "BLACK VIP 212",
+    "reference": "",
+    "family": "Amaderado",
+    "price": "85.000",
+    "photo": "catalogo/1000506159.webp"
+  },
+  {
+    "id": "7cjn4kur",
+    "name": "NAUTICA VOYAGE",
+    "reference": "",
+    "family": "CÃ­trico",
+    "price": "80.000",
+    "photo": "catalogo/1000506157.webp"
+  },
+  {
+    "id": "37r48g7w",
+    "name": "HUGO BOSS UNLIMITED",
+    "reference": "",
+    "family": "CÃ­trico",
+    "price": "80.000",
+    "photo": "catalogo/1000506074.webp"
+  },
+  {
+    "id": "rxjk946u",
+    "name": "LE MALE LE PARFUM",
+    "reference": "",
+    "family": "Amaderado",
+    "price": "100.000",
+    "photo": "catalogo/1000506072.webp"
+  },
+  {
+    "id": "hv6jakjd",
+    "name": "ILMIN FEMME",
+    "reference": "",
+    "family": "Floral",
+    "price": "85.000",
+    "photo": "catalogo/1000506070.webp"
+  },
+  {
+    "id": "doryrst9",
+    "name": "ILMIN ROSO",
+    "reference": "",
+    "family": "Floral",
+    "price": "85.000",
+    "photo": "catalogo/1000506068.webp"
+  },
+  {
+    "id": "mdqc3w9m",
+    "name": "FAKAR DORADA",
+    "reference": "",
+    "family": "FougÃ¨re",
+    "price": "110.000",
+    "photo": "catalogo/1000506058.webp"
+  },
+  {
+    "id": "jzx4nygr",
+    "name": "OUD FOR GREATNESS",
+    "reference": "",
+    "family": "Oriental",
+    "price": "120.000",
+    "photo": "catalogo/1000506029.webp"
+  },
+  {
+    "id": "8kt24u9b",
+    "name": "ASAD BOURBON",
+    "reference": "",
+    "family": "FougÃ¨re",
+    "price": "110.000",
+    "photo": "catalogo/1000506027.webp"
+  },
+  {
+    "id": "fao7a98h",
+    "name": "EMEER",
+    "reference": "",
+    "family": "Oriental",
+    "price": "110.000",
+    "photo": "catalogo/1000506009.webp"
+  },
+  {
+    "id": "jicqssse",
+    "name": "AFEEF",
+    "reference": "",
+    "family": "Oriental",
+    "price": "165.000",
+    "photo": "catalogo/1000506007.webp"
+  },
+  {
+    "id": "2ovg7vjq",
+    "name": "MANDARINE SKY ELIXIR",
+    "reference": "",
+    "family": "Floral",
+    "price": "90.000",
+    "photo": "catalogo/1000506005.webp"
+  },
+  {
+    "id": "ao62ehz0",
+    "name": "SHAHEEN",
+    "reference": "",
+    "family": "Oriental",
+    "price": "125.000",
+    "photo": "catalogo/1000505988.webp"
+  },
+  {
     "id": "5yi2f6vr",
     "name": "THANK U NEXT ( ARIANA GRANDE",
     "reference": "",
